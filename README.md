66eb139ad3b6b71736c6561cba3c54a0d196d8a0
khintz <kasper@hintz.xyz>
2020-10-30 08:00:21 +0100

    Added readme and cleaned up in code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # makePackage
+Small utility to set up new Julia packages.\
+It uses **PkgTemplates.jl** to generate the new package project and **argParse** to take input arguments.
+
+This utility is made so that I don't need to remember all the different relevant settings in **PkgTemplates.jl**. There will probably come more features along the way.
+
+## How to use?
+The main script is **src/makePackage.jl**
+Get a helping message by running either:
+- `julia makePackage.jl -h`
+- `julia makePackage.jl showExample`
+
+A new project can be created with the following example:\
+`julia makePackage.jl --packageName testjul --packageDir ~/git/ --user Duck --authors Duck --host github.com  makePackage`
+
+All flags have a default value so only the *makePackage* command is necessary.
--- src/makePackage.jl
+++ src/makePackage.jl
@@ -1,5 +1,84 @@
 module makePackage
 
-# Write your package code here.
+using ArgParse
+using PkgTemplates
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "makePackage"
+            help = "a positional argument to tell a new package should be created"
+            action = :command
+        "showExample"
+            help = "show examples of this program"
+            action = :command
+        "--packageName"
+            help = "Set the name of your package"
+            arg_type = String
+            default = "juliatest"
+        "--packageDir"
+            help = "Set the directory where the package should be placed"
+            arg_type = String
+            default = "~/git"
+        "--user"
+            help = "Set the user for the new project"
+            arg_type = String
+            default = "Donald"
+        "--authors"
+            help = "Set the authors for the new project"
+            arg_type = String
+            default = "Donald"
+        "--host"
+            help = "Set the host for the new project"
+            arg_type = String
+            default = "github.com"
+        # "--flag1"
+        #     help = "an option without argument, i.e. a flag"
+        #     action = :store_true
+        end
+
+ 
+    return parse_args(s)
+end
+
+
+function fmakePackage(packageName, packageDir, user, authors, host)
+    println("creating ",packageName)
+
+    t = Template(user=user, dir=packageDir, authors=authors, julia=v"1.5.2", host=host, 
+                    plugins=[
+                        License(; name="MIT", destination="LICENSE"),
+                        Git(; manifest=true, ssh=true)
+                        ])
+
+    t(packageName)
+
+end
+
+
+function main()
+    parsed_args = parse_commandline()
+    
+    if occursin("showExample", parsed_args["%COMMAND%"])
+        println("julia makePackage.jl --packageName testjul --packageDir ~/git/ --user Duck --authors Duck --host github.com  makePackage")
+        exit()
+    end
+
+
+    if occursin("makePackage", parsed_args["%COMMAND%"])
+        fmakePackage(parsed_args["packageName"], 
+                     parsed_args["packageDir"], 
+                     parsed_args["user"],
+                     parsed_args["authors"],
+                     parsed_args["host"])
+
+        exit()
+    end
+
+
+end
+
+main()
 
 end
